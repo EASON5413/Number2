d136ea4ddcca41e93667c4831fafa0c0b719a8b5
EASON5413 <eason880077@gmail.com>
2025-03-03 17:08:29 +0800

    Add files via upload

---
+++ apimaker3/Controllers/Class.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+[Route("api/Status")]
+[ApiController]
+public class ClassController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World");
+    }
+}
